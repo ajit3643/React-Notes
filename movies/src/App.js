2ad3c6443797bb682a2290911ef73612a4c87bd3
Kumar Ajit <ajitinfotech28@gmail.com>
2025-01-04 13:55:26 +0530

    Style Added

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,10 +1,12 @@
 import MovieList from "./MovieList";
 import Navbar from "./Navbar";
-export default function App() {
-  return (
+
+export default function App(){
+  return(
     <>
-      <Navbar />
-      <MovieList />
+    <Navbar />
+    <MovieList />
     </>
-  );
+
+  )
 }
