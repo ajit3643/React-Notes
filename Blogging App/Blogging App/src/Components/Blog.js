41c803a22f7fbc69b2377c9d8a09de310049412d
Kumar Ajit <ajitinfotech28@gmail.com>
2025-02-07 10:55:19 +0530

    Get all documents in a collection

--- Blogging App/Blogging App/src/Components/Blog.js
+++ Blogging App/Blogging App/src/Components/Blog.js
@@ -1,28 +1,191 @@
-//Blogging App using Hooks
-//Importing useState hook from react library
-import { useState, useRef, useEffect, useReducer } from "react";
-import { db } from "../firebaseinit";
-import { collection, doc, setDoc } from "firebase/firestore";
+// //Blogging App with Firebase
+// //Importing useState hook from react library
+// import { useState, useRef, useEffect, useReducer } from "react";
 
-//Reducer function to manage the state of the blogs
-function blogsReducer(state, action) {
-  switch (action.type) {
-    case "ADD_BLOG":
-      return [action.blog, ...state];
-    case "REMOVE_BLOG":
-      return state.filter((blog, index) => index !== action.index);
-    default:
-      return state;
-  }
-}
+// //Import fireStore reference from frebaseInit file
+// import { db } from "../firebaseinit";
+
+// //Import all the required functions from fireStore
+// import { collection, doc, getDocs, setDoc } from "firebase/firestore";
+
+// //Reducer function to manage the state of the blogs
+// function blogsReducer(state, action) {
+//   switch (action.type) {
+//     case "ADD_BLOG":
+//       return [action.blog, ...state];
+//     case "REMOVE_BLOG":
+//       return state.filter((blog, index) => index !== action.index);
+//     default:
+//       return state;
+//   }
+// }
+
+// export default function Blog() {
+//   //   const [title, setTitle] = useState("");
+//   //   const [content, setContent] = useState("");
+
+//   const [formData, setFormData] = useState({ title: "", content: "" });
+//   // const [blogs, setBlogs] = useState([]); //State variable to store the list of blogs
+//   const [blogs, dispatch] = useReducer(blogsReducer, []); //State variable to store the list of blogs
+//   const titleRef = useRef(null);
+
+//   useEffect(() => {
+//     titleRef.current.focus();
+//   }, []);
+
+//   useEffect(() => {
+//     /*********************************************************************** */
+//     /** get all the documents from the fireStore using getDocs() */
+//     /*********************************************************************** */
+//     async function fetchData() {
+//       const snapShot = await getDocs(collection(db, "blogs"));
+//       console.log(snapShot);
+
+//       const blogs = snapShot.docs.map((doc) => {
+//         return {
+//           id: doc.id,
+//           ...doc.data(),
+//         };
+//       });
+//       console.log(blogs);
+//     }
+
+//     fetchData();
+//     /*********************************************************************** */
+//   }, []);
+
+//   useEffect(() => {
+//     if (blogs.length && blogs[0].title) {
+//       document.title = blogs[0].title;
+//     } else {
+//       document.title = "No Blogs Available";
+//     }
+//   }, [blogs]);
+
+//   //Passing the synthetic event as argument to stop refreshing the page on submit
+//   async function handleSubmit(e) {
+//     e.preventDefault();
+
+//     //Creating a new blog object with title and content
+//     //setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+
+//     // Add a new document with a generated id.
+
+//     const docRef = await doc(collection(db, "blogs"));
+//     // setDoc....
+//     await setDoc(docRef, {
+//       title: formData.title,
+//       content: formData.content,
+//       createdOn: new Date(),
+//     });
+//     // console.log("Document written with ID: ", docRef.id);
+
+//     //Dispatching the action to add the blog
+//     dispatch({
+//       type: "ADD_BLOG",
+//       blog: { title: formData.title, content: formData.content },
+//     });
+
+//     setFormData({ title: "", content: "" });
+//     titleRef.current.focus();
+//     console.log(blogs);
+//   }
+
+//   //Function to remove the blog from the list
+//   function removeBlog(index) {
+//     // setBlogs(blogs.filter((blog, i) => i !== index));
+//     dispatch({ type: "REMOVE_BLOG", index });
+//   }
+//   return (
+//     <>
+//       {/* Heading of the page */}
+//       <h1>Write a Blog!</h1>
+
+//       {/* Division created to provide styling of section to the form */}
+//       <div className="section">
+//         {/* Form for to write the blog */}
+//         <form onSubmit={handleSubmit}>
+//           {/* Row component to create a row for first input field */}
+//           <Row label="Title">
+//             <input
+//               className="input"
+//               placeholder="Enter the Title of the Blog here.."
+//               value={formData.title}
+//               ref={titleRef}
+//               onChange={(e) =>
+//                 setFormData({
+//                   title: e.target.value,
+//                   content: formData.content,
+//                 })
+//               }
+//             />
+//           </Row>
+
+//           {/* Row component to create a row for Text area field */}
+//           <Row label="Content">
+//             <textarea
+//               className="input content"
+//               placeholder="Content of the Blog goes here.."
+//               value={formData.content}
+//               required
+//               onChange={(e) =>
+//                 setFormData({ title: formData.title, content: e.target.value })
+//               }
+//             />
+//           </Row>
+
+//           {/* Button to submit the blog */}
+//           <button className="btn">ADD</button>
+//         </form>
+//       </div>
+
+//       <hr />
+
+//       {/* Section where submitted blogs will be displayed */}
+//       <h2> Blogs </h2>
+//       {blogs.map((blog, index) => (
+//         <div className="blog" key={index}>
+//           <h3>{blog.title}</h3>
+//           <p>{blog.content}</p>
+//           <div className="blog-btn">
+//             <button className="remove" onClick={() => removeBlog(index)}>
+//               Delete
+//             </button>
+//           </div>
+//         </div>
+//       ))}
+//     </>
+//   );
+// }
+
+// //Row component to introduce a new row section in the form
+// function Row(props) {
+//   const { label } = props;
+//   return (
+//     <>
+//       <label>
+//         {label}
+//         <br />
+//       </label>
+//       {props.children}
+//       <hr />
+//     </>
+//   );
+// }
+
+//Blogging App with Firebase
+import { useState, useRef, useEffect } from "react";
+
+//Import fireStore reference from frebaseInit file
+import { db } from "../firebaseinit";
+
+//Import all the required functions from fireStore
+import { collection, doc, getDocs, setDoc } from "firebase/firestore";
 
 export default function Blog() {
-  //   const [title, setTitle] = useState("");
-  //   const [content, setContent] = useState("");
+  const [formData, setformData] = useState({ title: "", content: "" });
+  const [blogs, setBlogs] = useState([]);
 
-  const [formData, setFormData] = useState({ title: "", content: "" });
-  // const [blogs, setBlogs] = useState([]); //State variable to store the list of blogs
-  const [blogs, dispatch] = useReducer(blogsReducer, []); //State variable to store the list of blogs
   const titleRef = useRef(null);
 
   useEffect(() => {
@@ -30,65 +193,69 @@ export default function Blog() {
   }, []);
 
   useEffect(() => {
-    if (blogs.length && blogs[0].title) {
-      document.title = blogs[0].title;
-    } else {
-      document.title = "No Blogs Available";
-    }
-  }, [blogs]);
+    /*********************************************************************** */
+    /** get all the documents from the fireStore using getDocs() */
+    /*********************************************************************** */
+    async function fetchData() {
+      const snapShot = await getDocs(collection(db, "blogs"));
+      console.log(snapShot);
 
-  //Passing the synthetic event as argument to stop refreshing the page on submit
+      const blogs = snapShot.docs.map((doc) => {
+        return {
+          id: doc.id,
+          ...doc.data(),
+        };
+      });
+      console.log(blogs);
+      setBlogs(blogs);
+    }
+
+    fetchData();
+    /*********************************************************************** */
+  }, []);
+
   async function handleSubmit(e) {
     e.preventDefault();
+    titleRef.current.focus();
 
-    //Creating a new blog object with title and content
-    //setBlogs([{ title: formData.title, content: formData.content }, ...blogs]);
+    // Commenting setBlogs() as realtime Updates will be recieved from the database
+    //setBlogs([{title: formData.title,content:formData.content}, ...blogs]);
 
-    // Add a new document with a generated id.
+    /*********************************************************************** */
+    /** Add a new document with an auto generated id. */
+    /*********************************************************************** */
 
-    const docRef = await doc(collection(db, "blogs"));
-    // setDoc....
+    const docRef = doc(collection(db, "blogs"));
+
     await setDoc(docRef, {
       title: formData.title,
       content: formData.content,
       createdOn: new Date(),
     });
-    // console.log("Document written with ID: ", docRef.id);
 
-    //Dispatching the action to add the blog
-    dispatch({
-      type: "ADD_BLOG",
-      blog: { title: formData.title, content: formData.content },
-    });
+    /*********************************************************************** */
 
-    setFormData({ title: "", content: "" });
-    titleRef.current.focus();
-    console.log(blogs);
+    setformData({ title: "", content: "" });
   }
 
-  //Function to remove the blog from the list
-  function removeBlog(index) {
-    // setBlogs(blogs.filter((blog, i) => i !== index));
-    dispatch({ type: "REMOVE_BLOG", index });
+  async function removeBlog(i) {
+    setBlogs(blogs.filter((blog, index) => index !== i));
   }
+
   return (
     <>
-      {/* Heading of the page */}
       <h1>Write a Blog!</h1>
-
-      {/* Division created to provide styling of section to the form */}
       <div className="section">
         {/* Form for to write the blog */}
         <form onSubmit={handleSubmit}>
-          {/* Row component to create a row for first input field */}
           <Row label="Title">
             <input
               className="input"
               placeholder="Enter the Title of the Blog here.."
-              value={formData.title}
               ref={titleRef}
+              value={formData.title}
               onChange={(e) =>
-                setFormData({
+                setformData({
                   title: e.target.value,
                   content: formData.content,
                 })
@@ -96,20 +263,18 @@ export default function Blog() {
             />
           </Row>
 
-          {/* Row component to create a row for Text area field */}
           <Row label="Content">
             <textarea
               className="input content"
               placeholder="Content of the Blog goes here.."
-              value={formData.content}
               required
+              value={formData.content}
               onChange={(e) =>
-                setFormData({ title: formData.title, content: e.target.value })
+                setformData({ title: formData.title, content: e.target.value })
               }
             />
           </Row>
 
-          {/* Button to submit the blog */}
           <button className="btn">ADD</button>
         </form>
       </div>
@@ -118,12 +283,19 @@ export default function Blog() {
 
       {/* Section where submitted blogs will be displayed */}
       <h2> Blogs </h2>
-      {blogs.map((blog, index) => (
-        <div className="blog" key={index}>
+      {blogs.map((blog, i) => (
+        <div className="blog" key={i}>
           <h3>{blog.title}</h3>
+          <hr />
           <p>{blog.content}</p>
+
           <div className="blog-btn">
-            <button className="remove" onClick={() => removeBlog(index)}>
+            <button
+              onClick={() => {
+                removeBlog(i);
+              }}
+              className="btn remove"
+            >
               Delete
             </button>
           </div>
